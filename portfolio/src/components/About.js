35d1e159b7850d557892141d8bff8b7de901fa62
queeniewilliams <queeniepu329@gmail.com>
2021-03-12 17:02:33 -0800

    added projects

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -3,26 +3,9 @@ import '../css/About.css'
 import ScrollIntoView from 'react-scroll-into-view'
 import SlideDrawer from './SlideDrawer'
 import Backdrop from './Backdrop'
-import Footprint from './Footprint'
+import Map from './Map'
 
-// const About = () => {
-//   return (
-//     <div>
-//       <ScrollIntoView selector="#footer">
-//         <div className="section-1">
-//           <button className="mdl-button mdl-js-button mdl-button--raised">
-//             Jump to bottom
-//           </button>
-//         </div>
-//       </ScrollIntoView>
-//       <div id="footer">Scroll target element</div>
-//     </div>
-//   )
-// }
-
-// export default About
-
-export default class MainPage extends React.Component {
+export default class About extends React.Component {
   state = { drawerOpen: false }
   drawerToggleClickHandler = () => {
     this.setState({
@@ -53,10 +36,31 @@ export default class MainPage extends React.Component {
                   Queenie Williams
                 </h1>
                 <h3>Front-end Developer</h3>
-                <h2>Intro</h2>
+                <h4 style={{ marginTop: '40px' }}>
+                  I am an aspiring software engineer with an interest in
+                  JavaScript and the front-end of applications. My previous
+                  background in interior design brought me close to my true
+                  passion: building web applications. I wasn’t satisfied with
+                  only being involved in the visual aspect, and wanted a deeper
+                  involvement in building out applications. In the next phase of
+                  my career, I not only want to use my design skills to make the
+                  application look beautiful, but I also want to work on the
+                  technically challenging facets of the application too. I seek
+                  a role as a software developer at a company where I can learn,
+                  grow, and continue to master the craft.
+                </h4>
                 <img
                   src="https://i.ibb.co/ZLWQF3M/signature-copy.png"
                   width="300px"
+                />
+                <br></br>
+                <img
+                  src="https://i.ibb.co/ZJg2gCJ/kisspng-linkedin-computer-icons-business-advertising-compa-grey-background-5acab17806b152-7517612915.png"
+                  width="50px"
+                />
+                <img
+                  src="https://i.ibb.co/ZmChxdM/pngfind-com-github-png-2562232.png"
+                  width="47px"
                 />
               </div>
             </div>
@@ -78,7 +82,7 @@ export default class MainPage extends React.Component {
             <h3>My Footprint</h3>
           </div>
           <div id="map">
-            <Footprint />
+            <Map />
           </div>
         </div>
       </div>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,6 +1,85 @@
 import React from 'react'
+import '../css/project.css'
+import { NavLink } from 'react-router-dom'
 
 const Projects = () => {
-  return <div className="projects"></div>
+  return (
+    <div className="projects">
+      <NavLink
+        className="navlink"
+        to="/fincloud"
+        style={{ textDecoration: 'none' }}
+      >
+        <div className="project">
+          <img
+            src="https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png"
+            width="600px"
+          />
+
+          <div id="project-intro">
+            <p id="project-name">FinCloud</p>
+            <p id="text">
+              This full-stack development project allows an user to track
+              personal incomes and expenses either from a big-picture
+              perspective or in rich details.
+            </p>
+            <div id="icons">
+              <img
+                src="https://i.ibb.co/jbY89w4/design-development-facebook-framework-mobile-react-icon-1320165723839064798-512.png"
+                width="90px"
+              />
+              <img
+                src="https://i.ibb.co/KF8bKgD/install-javascript-js-node-npm-tools-icon-1320165731324625592-512.png"
+                width="90px"
+              />
+              <img
+                src="https://i.ibb.co/k8njgQK/mongodb-original-wordmark-1324760552967962880-512.png"
+                width="75px"
+                height="75px"
+              />
+              <img
+                src="https://kalvanaveen.github.io/WebDevelopmentResources.github.io/Images/Express-JS-min.png"
+                width="70px"
+                height="75px"
+              />
+            </div>
+          </div>
+        </div>
+      </NavLink>
+      <NavLink className="navlink" to="/p2" style={{ textDecoration: 'none' }}>
+        <div className="project">
+          <img
+            src="https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png"
+            width="600px"
+          />
+          <div id="project-intro">
+            <h2>Project 2</h2>
+          </div>
+        </div>
+      </NavLink>
+      <NavLink className="navlink" to="/p3" style={{ textDecoration: 'none' }}>
+        <div className="project">
+          <img
+            src="https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png"
+            width="600px"
+          />
+          <div id="project-intro">
+            <h2>Project 3</h2>
+          </div>
+        </div>
+      </NavLink>
+      <NavLink className="navlink" to="/p4" style={{ textDecoration: 'none' }}>
+        <div className="project">
+          <img
+            src="https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2017Q2/project-planning-header@2x.png"
+            width="600px"
+          />
+          <div id="project-intro">
+            <h2>Project 4</h2>
+          </div>
+        </div>
+      </NavLink>
+    </div>
+  )
 }
 export default Projects
